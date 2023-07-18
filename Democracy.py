7f30eb3b41c16510c1aea12769207f0e7b7fc69b
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2023-07-18 19:47:42 +0300

    Update and rename Democracy.py to Plot2.R

--- Democracy.py
+++
@@ -1,5 +0,0 @@
-# 0 =0 , 1 = o, 2 = oo 3 = boot , 4 = oooo, 5 = ooooo , 6 = oooooo, 7 = ooooooo, 8 = oooooooo, 9 = ooooooooo
-#Z = 4 = fuzz = diamond
-#In ancient Greece letter Z was the symbol of democracy, I cannot think of a more elegant or simple idea than this no matter how hard I tried. 
-
-"Z"
